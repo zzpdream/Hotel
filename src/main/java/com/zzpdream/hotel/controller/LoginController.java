fc3eac84b9ffb5b9ce800093357ce423428f32e0
zzpdream123 <zzpdream@qq.com>
2018-03-17 11:21:44 +0800

    完善登陆逻辑

--- src/main/java/com/zzpdream/hotel/controller/LoginController.java
+++ src/main/java/com/zzpdream/hotel/controller/LoginController.java
@@ -2,10 +2,12 @@ package com.zzpdream.hotel.controller;
 
 import com.zzpdream.hotel.dao.UserDao;
 import com.zzpdream.hotel.entity.ReturnData;
+import com.zzpdream.hotel.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -22,9 +24,18 @@ public class LoginController {
 
     @PostMapping("/login")
     @ResponseBody
-    public ReturnData login(@RequestBody String username,@RequestBody String password){
-        System.out.println("username:"+username+" password:"+password);
-        return new ReturnData(0,"sucess","1");
+    public ReturnData login(User user){
+        System.out.println("username:"+user.getName()+" password:"+user.getPassword());
+        List<User> users=userDao.findByName(user.getName());
+        if(users!=null&&users.size()>0){
+            if(user.getPassword().equals(users.get(0).getPassword())){
+                return new ReturnData(0,"sucess","1");
+            }
+            return new ReturnData(1,"密码错误","1");
+        }else{
+            return new ReturnData(1,"无该用户","1");
+        }
+
 //        return "redirect:/login.html";
     }
 }
--- src/main/java/com/zzpdream/hotel/dao/UserDao.java
+++ src/main/java/com/zzpdream/hotel/dao/UserDao.java
@@ -2,6 +2,13 @@ package com.zzpdream.hotel.dao;
 
 import com.zzpdream.hotel.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface UserDao extends JpaRepository<User,Integer> {
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
+    List<User> findByName(String name);
 }
